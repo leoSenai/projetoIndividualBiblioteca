f7052f76b39aecc9c9dcf88909bba1d618b6f5cc
Leonardo de Castro <leonardo_castro@estudante.sc.senai.br>
2022-03-14 21:52:13 -0300

    feat: adição toastr, jquery e imagem pos tiny png

---
+++ frontend/assets/js/login.js
@@ -0,0 +1,13 @@
+$('#usuario').on('focusout', function(event){
+    let user = $('#usuario').val();
+    if(user == ""){
+        toastr.error('Usuário não preenchido');
+    }
+});
+
+
+$('#logar').on('click', function(event){
+    event.preventDefault();
+    let user = $('#usuario').val();
+    let password = $('#password').val();
+});
