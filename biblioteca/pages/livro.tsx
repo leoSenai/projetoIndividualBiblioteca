dec33f706d6ee433fd3606167e2a1ea61d26ba55
Leonardo de Castro <leonardo_castro@estudante.sc.senai.br>
2022-04-18 20:35:36 -0300

    feat: Livro Terminado

--- biblioteca/pages/livro.tsx
+++ biblioteca/pages/livro.tsx
@@ -5,6 +5,8 @@ import Lista from '../components/lista';
 import Menu from '../components/menu';
 import PageBar from '../components/pageBar';
 import LivroModal from '../components/livroModal'
+import AlertToastr from '../components/alertToastr';
+import { LocalActivityRounded } from '@mui/icons-material';
 
 export default function Livro(){
 
@@ -20,13 +22,15 @@ export default function Livro(){
     const [id, setId] = useState(0);
     const [ISBN,setISBN] = useState("");
     const [titulo,setTitulo] = useState("");
-    const [numeroExemplares,setNumeroExemplares] = useState(0);
+    const [numeroExemplares,setNumeroExemplares] = useState("");
     const [editora,setEditora] = useState("");
     const [tema,setTema] = useState("");
-    const [categoria,setCategoria] = useState(0);
+    const [categoria,setCategoria] = useState("");
 
     const cookies = new Cookies()
     const access_token = cookies.get('access_token');
+
+    const handleClose = () => {setOpenToastr(false)}
 
     const changeBook = async () => {
         let alterOps = {
@@ -49,9 +53,15 @@ export default function Livro(){
 
         let ok = await fetch('api/livro/' + id, alterOps);
         if (ok.status != 200) {
-            console.log(ok);
+            setType(false);
+            setText("Ocorreu algum problema");
+            setOpenToastr(true);
+            setControlModalChange(false);
         }else{
-            console.log(ok);
+            setType(true);
+            setText("Alterado com sucesso");
+            setOpenToastr(true);
+            setControlModalChange(false);
         };
     }
     const modalChangeBook = async (selC: any) => {
@@ -78,9 +88,55 @@ export default function Livro(){
         setControlModalChange(true);
     }
     
+    const addBook = async () => {
+
+
+
+        let addOps = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "access_token": access_token,
+                "data":{
+                    "ISBN":ISBN,
+                    "titulo":titulo,
+                    "numeroExemplares":numeroExemplares,
+                    "editora":editora,
+                    "tema":tema,
+                    "categoria":categoria
+                }
+            }),
+        }
+        let livro =  await fetch('/api/livro/add', addOps);
+        if (livro.status != 200) {
+            setType(false);
+            setText("Ocorreu algum problema");
+            setOpenToastr(true);
+            setControlModalAdd(false);
+        }else{
+            setType(true);
+            setText("Adicionado com sucesso");
+            setOpenToastr(true);
+            setControlModalAdd(false);
+        };
+    } 
+
+    const modalAddBook = () => {
+        setId(0);
+        setISBN("");
+        setTitulo("");
+        setNumeroExemplares("");
+        setEditora("");
+        setTema("");
+        setCategoria("");
+        setControlModalAdd(true);
+    }
+
     return (<> 
         <Menu/>
-        <PageBar title="Livros" click={()=>{}}/>
+        <PageBar title="Livros" click={modalAddBook}/>
         <Lista 
             api="/livro/all" 
             dcop={modalChangeBook}
@@ -97,6 +153,16 @@ export default function Livro(){
             save={changeBook}
             close={()=> {setControlModalChange(false)}}
         />
+        <LivroModal 
+            key={2} 
+            title={"Adicionando Livro"}
+            dados={{ISBN,titulo, numeroExemplares, editora, tema, categoria}}
+            dadosF={{setISBN, setTitulo, setNumeroExemplares, setEditora, setTema, setCategoria}}
+            show={controlModalAdd} 
+            save={addBook}
+            close={()=> {setControlModalAdd(false)}}
+        />
+        <AlertToastr open={openToastr} type={type} close={handleClose} text={text}/>
         <Footer/>
      </>)
 }
