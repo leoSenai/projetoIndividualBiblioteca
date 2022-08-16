5f897e7fd861e325ea7db51e5a5a3d6accc5024c
Leonardo de Castro <leonardo_z@zyaon.com>
2022-08-16 20:09:59 -0300

    feat: guarda de rota terminado

--- biblioteca/components/routeGuard.jsx
+++ biblioteca/components/routeGuard.jsx
@@ -8,33 +8,29 @@ function RouteGuard({ children }) {
     const router = useRouter();
 
     useEffect(() => {
-        // on initial load - run auth check 
+        
         authCheck(router.asPath);
 
-        // on route change start - hide page content by setting authorized to false  
+        
 
-        // on route change complete - run auth check 
-        router.events.on('routeChangeComplete', authCheck)
+        
+        // router.events.on('routeChangeComplete', authCheck)
 
-        // unsubscribe from events in useEffect return function
-        return () => {
-            router.events.off('routeChangeComplete', authCheck);
-        }
+        // // unsubscribe from events in useEffect return function
+        // return () => {
+        //     router.events.off('routeChangeComplete', authCheck);
+        // }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     function authCheck(url) {
         // redirect to login page if accessing a private page and not logged in 
-        const publicPaths = ['/'];
         const path = url.split('?')[0];
         const cookies = new Cookies()
         const access_token = cookies.get('access_token');
-        if (access_token.length > 0 && !publicPaths.includes(path)) {
-            router.push({
-                pathname: '/',
-                query: { returnUrl: router.asPath }
-            });
+        if(!access_token && path != '/'){
+            router.push('/');
         }
     }
 
