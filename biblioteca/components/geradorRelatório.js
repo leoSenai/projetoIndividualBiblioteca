f82f97cb3b2abe5cef333e941280c3c824bb660a
Leonardo de Castro <leonardo_z@zyaon.com>
2022-08-18 21:32:00 -0300

    feat: base para gerar o relatorio

--- biblioteca/components/geradorRelatório.js
+++ biblioteca/components/geradorRelatório.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Autocomplete, TextField, Select, MenuItem, FormControl, Input } from "@mui/material";
 import * as toastr from 'toastr'
 import {Button} from 'react-bootstrap';
+import { jsPDF } from 'jspdf'
+
 export default function CreateReport(props) {
 
     let init = new Date()
@@ -19,15 +21,24 @@ export default function CreateReport(props) {
         setDataFim(event.target.value);
     }
 
-    const generateReport = () => {
+    const generateReport = async () => {
         let inicio= new Date(dataInicio);
         let fim = new Date(dataFim);
         if(inicio > fim){
             toastr.error("Período inválido", "Erro");
             return false;
         }
+        let report = new jsPDF({
+            orientation: 'p',
+            unit: 'cm',
+            format:'a4',
+        })
 
+        
+
+        report.save('teste.pdf')
     }
+
 
     return <>
         <div className='ta-center mt-4'>
